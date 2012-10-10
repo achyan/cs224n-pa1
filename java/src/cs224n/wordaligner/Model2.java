72eefc2f798452be68ae582e68cfde9b8999f480
achyan <alvin.chyan@gmail.com>
2012-10-09 22:35:23 -0700

    good enough Model 2

--- java/src/cs224n/wordaligner/Model2.java
+++ java/src/cs224n/wordaligner/Model2.java
@@ -1,5 +1,6 @@
 package cs224n.wordaligner;
 
+import java.util.HashMap;
 import java.util.List;
 
 import cs224n.util.Counter;
@@ -7,18 +8,31 @@ import cs224n.util.CounterMap;
 import cs224n.util.Pair;
 
 public class Model2 implements WordAligner {
+    private static final double THRESHOLD = 0.05; 
 
 	private CounterMap<String, String> eAndFCounts = new CounterMap<String, String>();
 	private Counter<String> eCounts = new Counter<String>();
 	private CounterMap<Pair<Integer, Integer>, Pair<Integer, Integer>> jGivenIlmCounts = new CounterMap<Pair<Integer, Integer>, Pair<Integer, Integer>>();
 	private Counter<Pair<Integer, Pair<Integer, Integer>>> ilmCounts = new Counter<Pair<Integer, Pair<Integer, Integer>>>();
 
+	private double lastAlignLikelihood = 0;
+	
+	
+//	private HashMap<String, Integer> targetDict = new HashMap<String,Integer>();
+//	private HashMap<String, Integer> sourceDict = new HashMap<String, Integer>();
+//	private int numSource = 0;
+//	private int numTarget = 0;
 	
 	@Override
 	public Alignment align(SentencePair sentencePair) {
+		lastAlignLikelihood = 0;
 		Alignment alignment = new Alignment();
 		
 		List<String> sourceSentence = sentencePair.getSourceWords(); //sourceSentence already has the null added during training
+		if(!sourceSentence.get(0).equals(NULL_WORD)) {
+			sourceSentence.add(0,NULL_WORD);
+		}
+		
 		List<String> targetSentence = sentencePair.getTargetWords();
 		int m_k = sourceSentence.size() - 1;  //don't count null
 		int l_k = targetSentence.size();
@@ -43,8 +57,9 @@ public class Model2 implements WordAligner {
 				}
 				sourceIndex++;
 			}
+			lastAlignLikelihood += Math.log(maxProb);
 			if (maxIndex != 0) {
-				alignment.addPredictedAlignment(targetIndex-1, maxIndex-1); //0 is <NULL>, which shifts everything down one; targetIndex was originally 1-indexed
+				alignment.addPredictedAlignment(targetIndex-1, maxIndex-1); //0 is <NULL>, which shifts everything down one; targetIndex was originally 1-indexed	
 			}
 			
 			targetIndex++;
@@ -65,7 +80,26 @@ public class Model2 implements WordAligner {
 		//preprocess
 		for (SentencePair sentencePair: trainingData) {
 			List<String> sourceWords = sentencePair.getSourceWords();
-			sourceWords.add(0,NULL_WORD);
+			if(!sourceWords.get(0).equals(NULL_WORD)) {
+				sourceWords.add(0,NULL_WORD);
+			}
+			
+//			for(String sourceWord : sourceWords) {
+//				if(!sourceDict.containsKey(sourceWord)) {
+//					sourceDict.put(sourceWord, numSource);
+//					numSource++;
+//				}
+//			}
+//			
+//			List<String> targetWords = sentencePair.getTargetWords();
+//
+//			for(String targetWord : targetWords) {
+//				if(!targetDict.containsKey(targetWord)) {
+//					targetDict.put(targetWord, numTarget);
+//					numTarget++;
+//				}
+//			}
+					
 		}
 		
 		//initialize to results of Model1
@@ -74,7 +108,17 @@ public class Model2 implements WordAligner {
 		CounterMap<Pair<Integer, Integer>, Pair<Integer, Integer>> prevJGivenIlmCounts = null;
 		Counter<Pair<Integer, Pair<Integer, Integer>>> prevIlmCounts = null;
 		
-		for (int iter = 0; iter < 10; iter++) {
+		double likelihood = 0;
+		double oldlikelihood = 0;
+		boolean isConverged = false;
+		double sum = 0;
+		int iter = 0;
+		while (!isConverged) {
+			iter++;
+			oldlikelihood = likelihood;
+			likelihood = 0;
+			
+		//for (int iter = 0; iter < 10; iter++) {
 			eAndFCounts = new CounterMap<String, String>();
 			eCounts = new Counter<String>();
 			jGivenIlmCounts = new CounterMap<Pair<Integer, Integer>, Pair<Integer, Integer>>();
@@ -94,7 +138,7 @@ public class Model2 implements WordAligner {
 					double qt = 0;
 					
 					//sum up all the qt values to get normalization factor
-					targetIndex = 1; //TODO: why target has null? shouldn't source have null?
+					targetIndex = 1; 
 					for(String targetWord : targetWords) {
 						if (prevJGivenIlmCounts == null) {
 							qt = 1;
@@ -110,9 +154,9 @@ public class Model2 implements WordAligner {
 						targetIndex++;
 					}
 					
-					targetIndex = 1; //TODO: why target has null? shouldn't source have null?
+					targetIndex = 1; 
 					for(String targetWord : targetWords) {
-						if (prevJGivenIlmCounts == null) {
+						if (prevJGivenIlmCounts == null) {  //first time running.  need to initialize qt
 							qt = 1;
 						}
 						else {
@@ -125,7 +169,7 @@ public class Model2 implements WordAligner {
 						delta = qt/normalizer;
 						
 						eAndFCounts.incrementCount(targetWord, sourceWord, delta);
-						eCounts.incrementCount(targetWord, delta);  //normalization purposes
+						eCounts.incrementCount(targetWord, delta);  //for normalization purposes
 						jGivenIlmCounts.incrementCount(new Pair<Integer,Integer>(targetIndex,sourceIndex),
 								new Pair<Integer,Integer>(l_k,m_k), delta);
 						ilmCounts.incrementCount(new Pair<Integer,Pair<Integer,Integer>>(sourceIndex,new Pair<Integer,Integer>(l_k,m_k)),delta);
@@ -143,6 +187,22 @@ public class Model2 implements WordAligner {
 			prevECounts = eCounts;
 			prevJGivenIlmCounts = jGivenIlmCounts;
 			prevIlmCounts = ilmCounts;
+			
+			
+			//test to see if converged
+			int sentencePairIndex = 0;
+			for (SentencePair sentencePair : trainingData) {
+				align(sentencePair);
+				likelihood += this.lastAlignLikelihood;
+				sentencePairIndex++;
+
+			}
+			System.out.println(iter + ":" + likelihood);
+			if(oldlikelihood != 0) {
+				if(likelihood < oldlikelihood || Math.abs(likelihood - oldlikelihood) < trainingData.size() * .05) {
+					isConverged = true;
+				}
+			}
 		}
 		
 	}
