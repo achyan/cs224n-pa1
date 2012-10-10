88d5f2e6a95012eab7277feb9857e59ca1eb492a
Justin Huang <jjhuang0@stanford.edu>
2012-10-09 20:00:21 -0700

    Model 1 again

--- java/src/cs224n/wordaligner/Model1.java
+++ java/src/cs224n/wordaligner/Model1.java
@@ -9,11 +9,13 @@ public class Model1 implements WordAligner {
 	private CounterMap<String, String> efCounter;
     private Counter<String> eCounter;
     private CounterMap<String, String> feProb;
+    private ArrayList<String> pairs;
     
     private final double THRESHOLD = 0.05; 
     
 	public Model1() {
     	feProb = new CounterMap<String,String>();
+    	pairs = new ArrayList<String>();
 	}
 	
 	public Alignment align(SentencePair sentencePair) {
@@ -58,10 +60,12 @@ public class Model1 implements WordAligner {
 			
 			for (String f : fWords) {
 				for (String e : eWords) {
-					feProb.setCount(f,e,0.2);
+					pairs.add(f+" "+e);
+					feProb.setCount(f,e,0.5);
 				}
 			}
 		}
+		
 		double delta = 0;
 		double likelihood = 0;
 		double oldlikelihood = 0;
@@ -88,13 +92,29 @@ public class Model1 implements WordAligner {
 			    		delta = feProb.getCount(f,e)/sum;
 			    		eCounter.incrementCount(e, delta);
 			    		efCounter.incrementCount(e,f, delta);
-			    		feProb.setCount(f,e,((double)efCounter.getCount(e,f)/eCounter.getCount(e)));
-			    	  	likelihood+=feProb.getCount(f,e);
 			    	}
 				}
 			}
 			
-			//System.out.println(Math.abs(oldlikelihood-likelihood));
+			for (String p : pairs) {
+				String[]fe = p.split(" ");
+	    		feProb.setCount(fe[0],fe[1],((double)efCounter.getCount(fe[1],fe[0])/eCounter.getCount(fe[1])));
+	    		likelihood+=feProb.getCount(fe[0],fe[1]);
+			}
+//			
+//			for (SentencePair pair : trainingData) { // For k = 1 ... n
+//				List<String> eWords = pair.getTargetWords();
+//			    List<String> fWords = pair.getSourceWords();
+//			    
+//			    for (String f : fWords) { // For i = 1 ... mk			    	
+//			    	for (String e : eWords) { // For j = 0 ... lk
+//			    		feProb.setCount(f,e,((double)efCounter.getCount(e,f)/eCounter.getCount(e)));
+//			    		likelihood+=feProb.getCount(f,e);
+//			    		System.out.println(f+","+e+": "+feProb.getCount(f,e));
+//			    	}
+//			    }
+//			}
+			System.out.println(Math.abs(oldlikelihood-likelihood));
 			if (Math.abs(oldlikelihood-likelihood) < THRESHOLD)
 				converge = true;
 		}
